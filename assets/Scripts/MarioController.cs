0b55550959d5a4fad29855ca7636a92e31e4b2af
tobbun <tobbun@gmail.com>
2015-05-13 19:24:02 +0200

    Added jumping, and a rudimentary sort of ground detection from some 2D character controller tut I found I can't even anymore, hello here have my work see what you can do with it.
    The sprinting as of right now is unreliable at best so heh have fun fixing that.

--- assets/Scripts/MarioController.cs
+++ assets/Scripts/MarioController.cs
@@ -3,29 +3,65 @@ using System.Collections;
 
 public class MarioController : MonoBehaviour {
 	public int speed;
-	public int jumpForce = 4;
-	public Rigidbody2D rb;
+	public int jumpForce;
 
-	public void Attributes(){
-		
+	private Rigidbody2D rb;
+
+	private Animator anim;
+	bool facingRight = true;
+
+	bool grounded = false;
+	public Transform groundCheck;
+	float groundRadius = 0.2f;
+	public LayerMask whatIsGround;
 	
-	}
-
 	void Awake(){
+		anim = GetComponent<Animator> ();
 		rb = GetComponent<Rigidbody2D>();
+
 	}
 	void FixedUpdate () {
-		speed = 4;
+
+		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
+		anim.SetBool ("isGrounded", grounded);
+
 		Move ();
-		Jump ();
+
+
 	}
 
-	void Move (){
-		transform.Translate (new Vector2(Input.GetAxis("Horizontal")*speed*Time.deltaTime,0));
-	}
-	void Jump(){
-		if(Input.GetButtonDown("Jump")){
-		rb.velocity = new Vector2(rb.velocity.x,jumpForce);
+	void Update(){
+
+		if(grounded == true && Input.GetButtonDown("Jump")){
+			anim.SetBool("isGrounded", false);
+			rb.velocity = (new Vector2(rb.velocity.x,jumpForce));
 		}
 	}
+	
+	void Move (){
+		float aSpeed = Input.GetAxis ("Horizontal");
+		if(aSpeed != 0 && Input.GetButtonDown ("Fire1")){
+			transform.Translate (new Vector2 ( aSpeed * speed * Time.deltaTime, 0));
+		} else if(aSpeed != 0){
+			transform.Translate (new Vector2 ( aSpeed * (speed * 1.4f)* Time.deltaTime, 0));
+		}
+
+		anim.SetFloat ("speed", Mathf.Abs (aSpeed));
+		if (aSpeed > 0 && !facingRight) {
+			Flip(-1);
+
+		} else if (aSpeed < 0 && facingRight){
+			Flip(1); 
+
+		}
+	}
+
+	void Flip(float move){
+		facingRight = !facingRight;
+		Vector2 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+		transform.Translate (new Vector2(move,0));
+	}
+
 }
