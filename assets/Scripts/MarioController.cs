b11598562ca4cb0e4dd49c16e6b037afebbf628f
tobbun <tobbun@gmail.com>
2015-05-13 13:04:08 +0200

    Added MarioController script and changed up his rigidbody to better serve the fizzicks. Next point: Improve the Horizontal Movement and add basic animations.

---
+++ assets/Scripts/MarioController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MarioController : MonoBehaviour {
+	public int speed;
+	public int jumpForce = 4;
+	public Rigidbody2D rb;
+
+	public void Attributes(){
+		
+	
+	}
+
+	void Awake(){
+		rb = GetComponent<Rigidbody2D>();
+	}
+	void FixedUpdate () {
+		speed = 4;
+		Move ();
+		Jump ();
+	}
+
+	void Move (){
+		transform.Translate (new Vector2(Input.GetAxis("Horizontal")*speed*Time.deltaTime,0));
+	}
+	void Jump(){
+		if(Input.GetButtonDown("Jump")){
+		rb.velocity = new Vector2(rb.velocity.x,jumpForce);
+		}
+	}
+}
